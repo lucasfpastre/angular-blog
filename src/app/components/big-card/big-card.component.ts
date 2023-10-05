2e0ed17f2a7084f20cc0f0bc1dde1de851aa879c
lucas.pastre <lucasfpastre@gmail.com>
2023-10-05 08:41:30 -0300

    Componentes dinâmicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+
+  @Input()
+  bookCover: string = ""
+  @Input()
+  bookTitle: string = ""
+  @Input()
+  bookIntro: string = ""
+  @Input()
+  bookAuthor: string = ""
+  @Input()
+  bookUrl: string = ""
+
+  ngOnInit(): void {
+
+  }
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+
+  @Input()
+  bookCover: string = "https://storage.googleapis.com/du-prd/books/images/9781649374042.jpg";
+  @Input()
+  bookTitle: string = "FOURTH WING";
+  @Input()
+  bookAuthor: string = "Rebecca Yarros";
+
+  ngOnInit(): void {
+
+  }
 
 }
